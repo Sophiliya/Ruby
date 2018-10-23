f8bdcc34202f2f47d254abdfe1b39bf56457ddeb
Sophiliya <44258264+Sophiliya@users.noreply.github.com>
2018-10-23 13:24:47 +0600

    Add files via upload
    Task #3

---
+++ task_3_1.rb
@@ -0,0 +1,8 @@
+puts 'Base:'
+base = gets.chomp.to_f
+
+puts 'Height:'
+height = gets.chomp.to_f
+
+s = 0.5 * base * height
+puts "The are of the triangle is #{s}."
---
+++ task_3_2.rb
@@ -0,0 +1,26 @@
+puts 'a ='
+a = gets.chomp.to_f
+
+puts 'b ='
+b = gets.chomp.to_f
+
+puts 'c ='
+c = gets.chomp.to_f
+
+array = [a,b,c].sort
+
+if array[2]**2 == array[0]**2 + array[1]**2
+
+  if array[0] == array[1]
+    puts 'Rectangular and equilateral!'
+  else
+    puts 'Rectangular!'
+  end
+
+elsif array[0] == array[1] && array[1] != array[2]
+  puts "Isosceles!"
+elsif array[0] == array[1] && array[1] == array[2]
+  puts "Equilateral!"
+else
+  puts "Just triangle!"
+end
