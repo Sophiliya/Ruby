9e8983873f93046432433104c4655d98a55d5f3e
Sophiliya <44258264+Sophiliya@users.noreply.github.com>
2018-10-25 10:49:44 +0600

    Update task_3_3.rb
    Исправила

--- task_3_3.rb
+++ task_3_3.rb
@@ -3,17 +3,21 @@ a = gets.chomp.to_f
 b = gets.chomp.to_f
 c = gets.chomp.to_f
 
-d = b**2 - 4*a*c
+d = b ** 2 - 4 * a * c
 puts 'Results:'
 puts "D = #{d}"
 
+d_sqrt = Math.sqrt(d)
+
 if d > 0
-  x1 = (-b + Math.sqrt(d))/(2*a)
-  x2 = (-b - Math.sqrt(d))/(2*a)
+  x1 = ( -b + d_sqrt ) / ( 2 * a )
+  x2 = ( -b - d_sqrt ) / ( 2 * a )
   puts "x1 = #{x1} and x2 = #{x2}."
+  
 elsif d == 0
   x = -b/(2*a)
   puts "x = #{x}."
+  
 else
   puts "The equation has no valid roots!"
 end
