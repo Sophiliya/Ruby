3ca7c225561c54350febdb092a1f4bbcb1aaf2cc
Sophiliya <44258264+Sophiliya@users.noreply.github.com>
2018-10-25 10:46:48 +0600

    Update task_3_1.rb
    Исправила

--- task_3_1.rb
+++ task_3_1.rb
@@ -4,5 +4,4 @@ base = gets.chomp.to_f
 puts 'Height:'
 height = gets.chomp.to_f
 
-s = 0.5 * base * height
-puts "The are of the triangle is #{s}."
+puts "The area of the triangle is #{0.5 * base * height}."
