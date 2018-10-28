5ee907eba1cb86b93dd1f62a0dc6faeb3626a4ca
Sophiliya <44258264+Sophiliya@users.noreply.github.com>
2018-10-28 12:07:14 +0600

    Update task_4_5.rb
    Исправила используя метод inject вместо for i do, убрала вычисления если месяц Январь и добавление одного дня если Февраль до 29-го числа (даже если високосный год)

--- task_4_5.rb
+++ task_4_5.rb
@@ -9,12 +9,13 @@ f_leap_year = ( year % 4 == 0 && year % 100 != 0 ) || ( year % 400 == 0 )
 
 sum_days = 0
 
-for i in 0..month - 2 do
-  if f_leap_year && i == 1
-    sum_days += 29
-  else
-    sum_days += days_in_months[i]
-  end
+if month == 1
+  puts "#{year}-#{month}-#{day} = #{day}"
+elsif f_leap_year && !( month ==2 && day < 29 )
+  days_in_months[1] = 29
+  sum_days = days_in_months[0..month-2].inject(0) { |sum, i| sum += i }
+  puts "#{year}-#{month}-#{day} = #{sum_days + day}"
+else
+  sum_days = days_in_months[0..month-2].inject(0) { |sum, i| sum += i }
+  puts "#{year}-#{month}-#{day} = #{sum_days + day}"
 end
-
-puts "#{year}-#{month}-#{day} = #{sum_days + day}"
