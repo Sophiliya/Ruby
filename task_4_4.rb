30919ab8434250a1071144cd327d2a41ce887e7b
Sophiliya <44258264+Sophiliya@users.noreply.github.com>
2018-10-28 12:03:46 +0600

    Update task_4_4.rb
    Невнимательно читала задание. Исправила, записала в хэш только гласные буквы как ключи.

--- task_4_4.rb
+++ task_4_4.rb
@@ -2,10 +2,14 @@ letters = ('a'..'z').to_a
 
 nums = (1..letters.length).to_a
 
+vowels = ['a','e','i','o','u']
+
 hash = {}
 
 for i in 0..letters.length - 1 do
-  hash [ letters[i].to_sym ] = nums[i]
+  if vowels.include? ( letters[i] )
+    hash [ letters[i].to_sym ] = nums[i]
+  end
 end
 
 hash.each { |k, v| puts "#{k} - #{v}" }
