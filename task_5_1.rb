6e64c0e04a18fa703e632ce4033f8efa2ecd06a0
Sophiliya <44258264+Sophiliya@users.noreply.github.com>
2018-11-01 00:14:42 +0600

    Update task_5_1.rb
    убрала объявление лишнего массива и итератора each после метода split

--- task_5_1.rb
+++ task_5_1.rb
@@ -1,7 +1,5 @@
 def find_max_min(string)
-  numbers = []
-  string.split.each { |i| numbers << string[i].to_f }
-  [numbers.max, numbers.min]
+  [string.split.max, string.split.min]
 end
 
-p find_max_min("12 34 -09 78 56 456 -797")
+p find_max_min("12 34 -9 78 56 456 -797")
