503b3ab2b26233f0fa7741195bdbfeca74aa2a62
Sophiliya <44258264+Sophiliya@users.noreply.github.com>
2018-11-19 21:22:24 +0600

    Update task_10.rb

--- task_10.rb
+++ task_10.rb
@@ -1,7 +1,8 @@
 def perform(command, data = [])
   if block_given?
     yield(command, data)
-  else p 'Please give me instructions.'
+  else
+    p 'Please give me instructions.'
   end
 end
 
@@ -9,27 +10,15 @@ strings = ['ruby python java scala oracle', 'math physics history']
 numbers = [1,2,3,4,5]
 
 perform('find words', strings) do |command, data|
-  if command == 'find words'
-    sizes = data.map { |s| s.split.size }
-    index = sizes.index(sizes.max)
-    p data[index]
-  else
-    result = {}
-    data.each_with_index { |e, i| result[i + 1] = e }
-    p result
-  end
+  sizes = data.map { |s| s.split.size }
+  index = sizes.index(sizes.max)
+  p data[index]
 end
 
 perform('parse numbers', numbers) do |command, data|
-  if command == 'find words'
-    sizes = data.map { |s| s.split.size }
-    index = sizes.index(sizes.max)
-    p data[index]
-  else
-    result = {}
-    data.each_with_index { |e, i| result[i + 1] = e }
-    p result
-  end
+  result = {}
+  data.each_with_index { |e, i| result[i + 1] = e }
+  p result
 end
 
-perform('find words',strings)
+perform('find words',strings) 
