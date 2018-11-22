e3e555f38dd63c6e57040e7f904f07ea3908a49f
Sophiliya <44258264+Sophiliya@users.noreply.github.com>
2018-11-22 12:29:26 +0600

    Update task_11.rb

--- task_11.rb
+++ task_11.rb
@@ -1,10 +1,10 @@
 class Station
-  def initialize(station)
-    @station = station
+  def initialize(name)
+    @name = name
   end
 
   def name
-    @station
+    @name
   end
 end
 
@@ -12,22 +12,23 @@ class Route
   def initialize(start, finish)
     @start = start
     @finish = finish
+    @stations = [@start.name, @finish.name]
   end
 
   def name
-    @start.name + ' - ' + @finish.name
+    "#{@start.name} - #{@finish.name}"
   end
 
   def stations
-    [@start.name, @finish.name]
+    @stations
   end
 end
 
 class Train
-  def initialize(number, type, wagons)
+  def initialize(number, type, wagons_count)
     @number = number
     @type = type
-    @wagons = wagons
+    @wagons_count = wagons_count
     @speed = 0
   end
 
@@ -43,16 +44,16 @@ class Train
     @speed = 0
   end
 
-  def length
-    @wagons
+  def wagons_count
+    @wagons_count
   end
 
   def hitching
-    @wagons += 1 if @speed == 0
+    @wagons_count += 1 if @speed == 0
   end
 
   def detaching
-    @wagons -= 1 if @speed == 0 && @wagons > 1
+    @wagons_count -= 1 if @speed == 0 && @wagons > 1
   end
 
-end
+end 
